a575629a8b8fc1a30b0ba1b4af49069cf69e3e3d
Thieen226 <t.ho1@my.metroed.net>
2024-01-31 10:03:19 -0800

    add

--- gradeManagement/main.swift
+++ gradeManagement/main.swift
@@ -29,14 +29,16 @@ catch{
 }
 
 //create variables
-var totalGrades : Int = 0
+var totalGrades : Double = 0
 var averageGrade : Double = 0.0
+var studentNames : [String] = []
 var allGrades : [String] = []
 var numOfAssignment : Int = 0
 var lowestGrade : Int = 0
 var highestGrade : Int = 0
 var displayMenu = true
 
+//when displayMenu is true, keeps showing the menu options
 while displayMenu{
     print("Welcome to the Grade Manager! \n"
         + "What would you like to do? (Enter the number): \n"
@@ -53,13 +55,13 @@ while displayMenu{
     if let userInput = readLine(){
         switch userInput{
         case "1":
-            gradeOfOneStudent()
+            displayStudentGrades(showAverage: true)
 
         case "2":
-            allGradesOfOneStudent()
+            displayStudentGrades(showAverage: false)
             
         case "3":
-            allGradesOfAllStudents()
+            allGradesOfAllStudents(showAverage: false)
             
         case "4":
             averageGradeOfClass()
@@ -78,6 +80,8 @@ while displayMenu{
             
         case "9":
             print("Have a great rest of your day!")
+            
+            //when the user choose option 9, stop showing menu options
             displayMenu = false
             
         default:
@@ -87,21 +91,53 @@ while displayMenu{
     }
 }
 
-func gradeOfOneStudent(){
+//making a function with parameter showAverage to separate case 1 and 2
+func displayStudentGrades(showAverage: Bool){
     print("Which student would you like to choose?")
     
     if let studentChosen = readLine(){
         
+        //use for loop to access names of students inside the 2d array
         for names in studentNamesAndGrades.indices{
+            
+            //check if the student's name input match the student's name in the 2d array
+            //make sure if the user enter lower case names, they are still able to get the correct names
             if studentChosen.lowercased() == studentNamesAndGrades[names][0].lowercased(){
+                
+                //reset variables for each student
+                totalGrades = 0
+                numOfAssignment = 0
+                allGrades = []
+                
+                //use for loop to access the student's grades
                 for grades in 1..<studentNamesAndGrades[names].count{
+                    
+                    //create a variable that stores all the grades of the student
                     if let studentGrades = Int(studentNamesAndGrades[names][grades]){
-                        totalGrades += studentGrades
+                        //add each grade together
+                        totalGrades += Double(studentGrades)
+                        
+                        //this variable keeps track the num of assignemnts
+                        //when the for loop goes through each grade, then add 1 to the variable
                         numOfAssignment += 1
+                        
+                        //append all the grades into the array
+                        allGrades.append(studentNamesAndGrades[names][grades])
                     }
                 }
-                averageGrade = Double(totalGrades/numOfAssignment)
-                print("\(studentChosen)'s grade in the class is \(averageGrade)")
+                averageGrade = totalGrades/Double(numOfAssignment)
+                
+                //create a variable to store all the grades of a student
+                //then convert the array into a string and separate each grade with ,
+                let showAllGrades = allGrades.joined(separator: ", ")
+                
+                if showAverage{
+                    print("\(studentChosen)'s grade in the class is \(averageGrade)")
+                }
+                else{
+                    print("\(studentChosen.capitalized)'s grades in this class are: \n"
+                          + "\(showAllGrades)")
+                }
                 return
             }
         }
@@ -109,32 +145,21 @@ func gradeOfOneStudent(){
     }
 }
 
-func allGradesOfOneStudent(){
-    print("Which student would you like to choose?")
-    
-    if let studentChosen = readLine(){
-        
-        for names in studentNamesAndGrades.indices{
-            if studentChosen.lowercased() == studentNamesAndGrades[names][0].lowercased(){
-                for grades in 1..<studentNamesAndGrades[names].count{
-                        allGrades.append(studentNamesAndGrades[names][grades])
-                }
-                let showAllGrades = allGrades.joined(separator: ", ")
-                print("\(studentChosen).uppercased()'s grades in this class are: \n"
-                      + "\(showAllGrades)")
-                
-            }
-        }
-        
-    }
-    
-}
 
-func allGradesOfAllStudents(){
-    
+func allGradesOfAllStudents(showAverage: Bool){
+    //use for loop to access each name and grade
+    for names in studentNamesAndGrades.indices{
+        let allNames = studentNamesAndGrades[names][0]
+        let allGrades = studentNamesAndGrades[names][1...].joined(separator: ", ")
+        print("\(allNames) grades are: \(allGrades)")
+    }
 }
 
 func averageGradeOfClass(){
+    for grades in studentNamesAndGrades.indices{
+        let grades = studentNamesAndGrades[grades][1...].joined(separator: ", ")
+        /*totalGrades = Double(grades)*/ //error
+    }
     
 }
 
